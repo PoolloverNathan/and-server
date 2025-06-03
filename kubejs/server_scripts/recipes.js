d9c49decc378ae2585634786591abd3ef73c287b
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2025-06-03 12:07:10 -0400

    m

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -1,4 +1,4 @@
 ServerEvents.recipes(event => {
   event.recipes.create.haunting("kubejs:soul_gravel", "minecraft:gravel")
-  event.recipes.create.crushing([Item.of("create:cinder_flour").withChance(0.3), Item.of("minecraft:lapis_lazuli").withChance(0.05)], "kubejs:soul_gravel")
+  event.recipes.create.washing([Item.of("create:cinder_flour").withChance(0.3), Item.of("minecraft:lapis_lazuli").withChance(0.05)], "kubejs:soul_gravel")
 })
