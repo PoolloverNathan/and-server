f5514b3c9ff96c18101eb847b6241cf65482e6ac
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2025-06-04 15:04:36 -0400

    recipe fixes, jade

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -1,4 +1,9 @@
 ServerEvents.recipes(event => {
   event.recipes.create.haunting("kubejs:soul_gravel", "minecraft:gravel")
-  event.recipes.create.washing([Item.of("create:cinder_flour").withChance(0.3), Item.of("minecraft:lapis_lazuli").withChance(0.05)], "kubejs:soul_gravel")
+  event.recipes.create.splashing([Item.of("create:cinder_flour").withChance(0.3), Item.of("minecraft:lapis_lazuli").withChance(0.05)], "kubejs:soul_gravel")
+  event.replaceInput(
+    { input: 'minecraft:crafting_table' },
+    'minecraft:crafting_table',
+    '#c:workbench',
+  )
 })
