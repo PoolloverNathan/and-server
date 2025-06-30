ea4739b9d7a2a17044fe496fd11efb46fb427cca
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2025-06-30 14:13:13 -0400

    what if auto backup

--- dedicated-server/server-start.sh
+++ dedicated-server/server-start.sh
@@ -3,4 +3,10 @@ cp /app/unsup.ini .
 java -jar /app/unsup.jar server
 echo "eula=true" > eula.txt
 [ -d world ] || btrfs sub create world
+mkdir -p backups
+t=$(date -Iseconds -u)
+mkdir -p backups/$t
+for d in world*; do
+	btrfs sub snap -r $d backups/$t/$d
+done
 exec java -Xms6G -Xmx6G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar /app/fabric-server.jar nogui "\$@"
